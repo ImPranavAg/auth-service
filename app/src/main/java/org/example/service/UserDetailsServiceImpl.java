da0fa0138180487f08289e2beea8e5202412fa02
ImPranavAg <pranavkrj2012@gmail.com>
2025-09-01 00:45:49 +0530

    setup the model-service as of now

---
+++ app/src/main/java/org/example/service/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package org.example.service;
+
+import org.example.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
